bd2fc6fbc3e210cb93d08574be46669adc7c9714
Austin <90881695+Cinderblook@users.noreply.github.com>
2022-04-26 23:15:05 -0400

    azapi

---
+++ Terraform/Azure/learning-azure/azure-azapi/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "=0.1.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.2"
+    }
+  }
+}
+
+provider "azapi" {
+  default_location = "eastus"
+  default_tags = {
+    team = "Azure deployments"
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "qs101" {
+  name     = "rg-qs101"
+  location = "westus2"
+}
